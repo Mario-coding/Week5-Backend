cdcc5f0e3fd420ccbc6529669bfef544a6871738
Mario-coding <laurenmario19@gmail.com>
2020-11-13 13:25:11 +0700

    message

--- src/app.ts
+++ src/app.ts
@@ -11,30 +11,6 @@ app.use(cors({origin: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/app/index', (req,res) => {
-    console.log ("req: ", req);
-    res.send("Method GET jalan!");
-    return res.status(200).json({message:"success!"});
-})
-
-app.post('/app/create', (req,res) => {
-    console.log ("req: ", req);
-    res.send("Method POST jalan!");
-    return res.status(200).json({message:"success!"});
-})
-
-app.delete('/app/remove', (req,res) => {
-    console.log ("req: ", req);
-    res.send("Method DELETE jalan!");
-    return res.status(200).json({message:"success!"});
-})
-
-app.put('/app/update', (req,res) => {
-    console.log ("req: ", req);
-    res.send("Method PUT jalan!");
-    return res.status(200).json({message:"success!"});
-})
-
 const server = http.createServer(app);
 function startServer(){
     server.listen(config.port, parseInt(config.host), function(){
