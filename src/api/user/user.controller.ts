37d6f7c9ac491ea17198fadc24528f543d4094a8
Mario-coding <laurenmario19@gmail.com>
2020-11-25 00:01:13 +0700

    message

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,21 +1,46 @@
 import { Request, Response } from 'express';
+import User from './user.model';
+
 
 export const index = async (req: Request, res: Response) => {
     console.log("req: ", req.body);
-    return res.status(200).json({message:"success!"})
+    try{
+        let users = await User.find({});
+        return res.status(200).json({data: users});
+      }catch(err){
+        return res.status(500).json({message: err})
+    }
 }
 
 export const create = async (req: Request, res: Response) => {
-    console.log("req: ", req.body);
-    return res.status(200).json({message:"success!"})
+    try{
+        let body = req.body;
+        let response = await User.create(body);
+        return res.status(201).json(response);
+      }catch(err){
+        return res.status(500).json({message: err})
+      }
 }
 
-export const remove = async (req: Request, res: Response) => {
-    console.log("req: ", req.body);
-    return res.status(200).json({message:"success!"})
-}
+export const remove = async(req: Request, res: Response) => {
+    try{
+      let id = req.params.id;
+      let body = req.body;
+      let response = await User.findByIdAndDelete(id, body);
+      return res.status(200).json(response);
+    }catch(err){
+      return res.status(500).json({message: err})
+    }
+  }
 
-export const update = async (req: Request, res: Response) => {
+  export const update = async(req: Request, res: Response) => {
     console.log("req: ", req.body);
-    return res.status(200).json({message:"success!"})
-}
+    try{
+      let id = req.params.id;
+      let body = req.body;
+      let response = await User.findByIdAndUpdate(id, body);
+      return res.status(201).json(response);
+    }catch(err){
+      return res.status(500).json({message: err})
+    }
+  }
